9fe774004349f04a1ae18ae8221ac0dd22dbef58
Kognise <felix.mattick@gmail.com>
2019-08-31 19:34:56 -0400

    Update Member.js

--- components/Member.js
+++ components/Member.js
@@ -26,16 +26,6 @@ export default (props) => (
         margin-top: 10px;
         text-align: center;
       }
-      @media only screen and (max-width: 696px) {
-        figure {
-          display: ${props.hidden === 'md' ? 'none' : 'block'};
-        }
-      }
-      @media only screen and (max-width: 400px) {
-        figure {
-          display: ${props.hidden === 'sm' ? 'none' : 'block'};
-        }
-      }
     `}</style>
   </figure>
 )
